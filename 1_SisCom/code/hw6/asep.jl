58303f79266b5f6427b03ae59198e70e4795a4f8
FranVT <fjrvazqueztf@gmail.com>
2024-04-09 20:50:08 -0600

    I get at assigning the probabilities of the particles that can go back and forw, particles that can fo for and particles that can go back.

--- 1_SisCom/code/hw6/asep.jl
+++ 1_SisCom/code/hw6/asep.jl
@@ -4,9 +4,10 @@
 """
 
 # Packages and stuff
-using Plots, Random
+using Plots, Random, Distributions
 
 # Setting the sead and the random number generator
+
 rgn = Random.Xoshiro(1234);
 
 # Define the parameters
@@ -16,12 +17,14 @@ rgn = Random.Xoshiro(1234);
     Np:     Number of particles
     α:      Rate to move forward
     β:      Rate to move backward
+    ϕ:      Possible change 
 """
-Nb = 100;
-η = 1/10;
-Np = Int(η*Nb);
+Nb = 10;
+η = 3/4;
+Np = ceil(Int64,η*Nb);
 α = 3;
 β = 2;
+ϕ = [-1,0,1];
 
 # Discretize the values
 """
@@ -35,4 +38,58 @@ dt = 1e-2;
 space = zeros(Np,1);
 
 # Initial position
-wsample([0,1],[1-η,η],Nb);
+"""
+    σ:      State 
+"""
+σ = wsample([0,1],[1-η,η],Nb);
+
+
+# Get the probabilities of change place
+"""
+   ids:     Ids of the particles
+   aux1:    Ids of particles that move forward
+   aux2:    Ids of particles that move backward
+   mf:      Particles that can move forward 
+   mb:      Particles that can move backward
+"""
+ids = findall(s->s==1,σ);
+aux1 = ids.+1;
+aux2 = ids.-1;
+
+# Periodic boundary condition
+aux1[aux1.>Nb].=1;
+aux2[aux2.<1].=Nb;
+
+mf = setdiff(aux1,ids).-1;
+# Periodic Boundary condition
+mf[mf.<1].=Nb;
+
+mb = setdiff(aux2,ids).+1;
+# Periodic boundary condition
+mb[mb.>Nb].=1;
+
+# Define the probabilities for each particles 
+"""
+    mfb:    Particles that can move forward and backward
+    mf:     Particles that can move forward 
+    mb:     PArticles that can move backward
+"""
+mfb = intersect(mf,mb);
+mf = setdiff(mf,mfb);
+mb = setdiff(mb,mfb);
+
+# Assign the step 
+fb = wsample([-1 0 1],[β*dt,1-(β+α)*dt,α*dt],length(mfb));
+f = wsample([0 1],[1-α*dt,α*dt],length(mf));
+b = wsample([-1 0],[β*dt,1-β*dt],length(mb));
+
+
+"""
+aux1 = copy(σ);
+aux2 = copy(σ);
+permute!(aux1,append!([Nb],collect(1:Nb-1)));
+permute!(aux2,append!(collect(2:Nb),[1]));
+pjf = α*dt*iszero.(aux1-σ);
+pjb = β*dt*iszero.(aux2-σ);
+pnj = 1.-(pjf+pjb);
+"""
