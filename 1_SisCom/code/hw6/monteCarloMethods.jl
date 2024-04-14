5982a8462a8a6eea2e5cb33efd276fc8d8eae87a
FranVT <fjrvazqueztf@gmail.com>
2024-04-14 11:02:03 -0600

    Finish the 6 homework

--- 1_SisCom/code/hw6/monteCarloMethods.jl
+++ 1_SisCom/code/hw6/monteCarloMethods.jl
@@ -14,31 +14,56 @@ gr()
 # Set the seed
 Random.seed!(4321)
 
-# Set parameters
-"""
-    r:      Radius of the circle
-    Ns:     Number of samples
-    srng:     Sample range 
-    spdf:   Sample probability function
-"""
-r = 1;
+function estimatePiMC(r,Ns)
+    # Set parameters
+    """
+        r:      Radius of the circle
+        Ns:     Number of samples
+        srng:     Sample range 
+        spdf:   Sample probability function
+    """
+    srng  = round.(Int,range(start=10,stop=1e6,length=Ns));
+    spdf = Uniform(-r,r);
+
+    # Evaluate the random varaibles
+    X = map(s->rand(spdf,s),srng);
+    Y = map(s->rand(spdf,s),srng);
+
+    # Take the radius
+    R = map(s->(X[s].^2 .+ Y[s].^2),1:1:Ns);
+
+    # Get the estimated value of pi
+    return (map(s->sum(R[s].<=r.^2)/srng[s],1:1:Ns ).*4, srng) 
+
+end
+
+r = 0.5;
 Ns = 2^8;
-srng  = round.(Int,range(start=10,stop=1e6,length=Ns));
-spdf = Uniform(-1,1);
+#info = estimatePiMC(r,Ns);
 
-# Evaluate the random varaibles
-X = map(s->rand(spdf,s),srng);
-Y = map(s->rand(spdf,s),srng);
-
-# Take the radius
-R = map(s->(X[s].^2 .+ Y[s].^2),1:1:Ns);
-
-# Get the estimated value of pi
-Pi = map(s->sum(R[s].<=r.^2)/srng[s],1:1:Ns ).*4; 
+# Get the error
+rpi = round(pi;digits=12);
+re = abs.(rpi.-round.(info[1];digits=12))/rpi;
 
 # Plot of the error
-ge = plot()
-plot!(ge,abs.(pi.-Pi))
+ge = plot(
+          title = L"\mathrm{Relative~error}",
+          xlabel = L"N_s:\mathrm{Number~of~samples}",
+          ylabel = L"\mathrm{Relative~error}",
+          xlim = (info[2][1],info[2][end]),
+          ylim = (0,maximum(re)),
+          size = (480,480),
+          #aspect_ratio = 1/1,
+          formatter = :plain,
+          framestyle = :box,
+          minorgrid = true,
+          minorgridalpha = 0.25,
+          gridalpha = 0.5
+         )
+plot!(ge,info[2],re,label = false)
+scatter!(ge,info[2],re,label=false,markersize=1.5,markercolor=:black)
+
+savefig(ge,"relativeError.pdf")
 
 # Plot the estimation
 gp = plot()
