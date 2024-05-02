c6af863f10b0f1d4950ec2f7893d55620e1e8628
FranVT <fjrvazqueztf@gmail.com>
2024-05-02 15:14:54 -0600

    changes in parameters

--- 1_SisCom/code/hw7f/parameters.jl
+++ 1_SisCom/code/hw7f/parameters.jl
@@ -12,23 +12,23 @@
     kb:     Bolztamnn constant
     T:      Temperature
 """
-Ng = 2^3;
+Ng = 2^4;
 σs = [-1,1];
 η = 0.5;
 J = 1;
 B = 1;
 kb = 1; 
-T = range(start=1.3,stop=3,length=100);
+T = range(start=1.3,stop=10,length=100);
 
 # Parameters for the Metropoli algorithm
 """
     Nsteps:   Number of cicles in the algorithm
     setSeeds: Set of seeds for every step  
 """
-Nsteps = 100;
+Nsteps = 20;
 
 # Parameter for the amount of simulations per termperature
-Nexp = 100;
+Nexp = 30;
 
 # Path to save the information
 path = string("/home/Fran/gitRepos/NanoTech-Masters/1_SisCom/data/data_hk7p/");
