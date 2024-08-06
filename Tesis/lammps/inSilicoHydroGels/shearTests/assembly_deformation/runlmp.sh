3850b8db7bbef3a13e637984b83c12a7c6a01a59
FranVT <vazqueztfj@proton.me>
2024-08-06 12:01:52 -0600

    Update the scripts for deformation simulation. Missing atoms in shear deformation. Change the time differential and strange stuff happends

---
+++ Tesis/lammps/inSilicoHydroGels/shearTests/assembly_deformation/runlmp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to run the input file
+
+#env OMP_RUN_THREADS=2 mpirun -np 8 lmp -sf omp -in in.assemblyShear.lmp
+#echo -e '\n'
+#echo -e 'End of the assembly process\n'
+echo -e 'Start the deformation process\n'
+echo -e '\n'
+env OMP_RUN_THREADS=2 mpirun -np 8 lmp -sf omp -in in.deformationShear.lmp
