6fea90022bf2ca2b586c9ab5bc96a254ef9fcae7
FranVT <vazqueztfj@proton.me>
2024-09-06 10:59:59 -0600

    First test of different relax times

--- Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/moveAux.sh
+++ Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/moveAux.sh
@@ -2,8 +2,8 @@
 
  # Move data files to system directory
 
-mv data.hydrogel /home/franpad/Documents/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
+mv data.hydrogel /home/franvtdebian/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
 cd info;
-mv energy_assembly.fixf /home/franpad/Documents/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
-mv energy_shear.fixf /home/franpad/Documents/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
-mv stressVirial_shear.fixf /home/franpad/Documents/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
+mv energy_assembly.fixf /home/franvtdebian/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
+mv energy_shear.fixf /home/franvtdebian/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
+mv stressVirial_shear.fixf /home/franvtdebian/GitRepos/NanoTech-Masters/Tesis/lammps/inSilicoHydroGels/experiments/TotalFree/dataFiles/systemCL200MO1800ShearRate2000Cyles4;
