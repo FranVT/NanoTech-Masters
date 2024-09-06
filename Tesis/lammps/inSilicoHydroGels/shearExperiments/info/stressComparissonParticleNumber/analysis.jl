735fd4c4ef677b8e1311834b36dc890a7823c3ab
FranVT <vazqueztfj@proton.me>
2024-09-05 18:03:13 -0600

    Create figures for comparisson with different particles numbers

---
+++ Tesis/lammps/inSilicoHydroGels/shearExperiments/info/stressComparissonParticleNumber/analysis.jl
@@ -0,0 +1,164 @@
+"""
+    Graphics for Stress
+"""
+
+using Distributions
+using GLMakie
+
+# Directories
+main_Dir = pwd();
+pwdDir = main_Dir; #string(main_Dir,"/info/");
+
+include("functions.jl")
+
+println("Main directory of analysis.jl: ", main_Dir)
+
+## Gather information
+
+workdir = cd(pwdDir);
+
+# Get information
+shearFiles_names = ("stressVirial_shear_2000.fixf","stressVirial_shear_4000.fixf","stressVirial_shear_6000.fixf");
+
+data_stress = map(s->getInfoStress(joinpath(pwdDir,s)),shearFiles_names);
+
+## Prepare the data
+function tensorNorm(stress_info)
+"""
+    Compute the stress norm of a symetrix tensor given 6 components.
+"""
+    normTensor = sum(abs.(stress_info[3:5,:]).^2,dims=1) .+ (2).*sum(abs.(stress_info[6:8,:]).^2,dims=1);
+    return Iterators.flatten(sqrt.(normTensor))|>collect;
+end
+
+strain = data_stress[1][2,:];
+
+data_norm = mapreduce(s->tensorNorm((-1).*s),hcat,data_stress)';
+
+data_T_xx = (-1).*mapreduce(s->s[3,:],hcat,data_stress)';
+
+data_T_xy = (-1).*mapreduce(s->s[6,:],hcat,data_stress)';
+
+
+## Prepare labels and stuff
+labels_stress = (L"2000 \mathrm{Particles}",L"4000 \mathrm{Particles}",L"6000 \mathrm{Particles}");
+
+### Figures
+fig_Stress = Figure(size=(1200,920));
+ax_s1 = Axis(fig_Stress[1,1:2],
+        aspect = nothing,
+        title = L"xx~\mathrm{Component}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+ax_s2 = Axis(fig_Stress[2,1:2],
+        aspect = nothing,
+        title = L"xy~\mathrm{Component}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+ax_s3 = Axis(fig_Stress[3,1:2],
+        aspect = nothing,
+        title = L"\mathrm{Norm}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+
+    series!(ax_s1,strain,data_T_xx,labels=labels_stress,color=:tab10)
+    axislegend(ax_s1,"Legends",position=:lb)
+
+    series!(ax_s2,strain,data_T_xy,labels=labels_stress,color=:tab10)
+    axislegend(ax_s2,"Legends",position=:lb)
+
+    series!(ax_s3,strain,data_norm,labels=labels_stress,color=:tab10)
+    axislegend(ax_s3,"Legends",position=:lb)
+
+fig_Stress
+
+save("Comparisson.png",fig_Stress)
+
+## Antoher graph
+## Prepare labels and stuff
+labels_stress2 = (L"T_xx",L"T_xy",L"\mathrm{Norm}");
+
+particles2 = mapreduce(s->s[1,:],hcat,(data_T_xx,data_T_xy,data_norm))';
+particles4 = mapreduce(s->s[2,:],hcat,(data_T_xx,data_T_xy,data_norm))';
+particles6 = mapreduce(s->s[3,:],hcat,(data_T_xx,data_T_xy,data_norm))';
+
+### Figures
+fig_Stress2 = Figure(size=(1200,920));
+ax_s12 = Axis(fig_Stress2[1,1:2],
+        aspect = nothing,
+        title = L"2000~\mathrm{Particles}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+ax_s22 = Axis(fig_Stress2[2,1:2],
+        aspect = nothing,
+        title = L"4000~\mathrm{Particles}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+ax_s32 = Axis(fig_Stress2[3,1:2],
+        aspect = nothing,
+        title = L"6000~\mathrm{Particles}",
+        xlabel = L"\mathrm{Tilt~deformation}",
+        ylabel = L"\sigma_{nn}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+    )
+
+    series!(ax_s12,strain,particles2,labels=labels_stress2,color=:tab10)
+    axislegend(ax_s1,"Legends",position=:lb)
+
+    series!(ax_s22,strain,particles4,labels=labels_stress2,color=:tab10)
+    axislegend(ax_s2,"Legends",position=:lb)
+
+    series!(ax_s32,strain,particles6,labels=labels_stress2,color=:tab10)
+    axislegend(ax_s3,"Legends",position=:lb)
+
+fig_Stress2
+
+save("Comparisson2.png",fig_Stress2)
+
+
