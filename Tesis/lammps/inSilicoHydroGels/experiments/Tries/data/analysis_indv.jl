df8544d0df46d159b4ce5b783e0845eacc6924fc
FranVT <vazqueztfj@proton.me>
2024-10-07 17:16:27 -0600

    Graphs

--- Tesis/lammps/inSilicoHydroGels/experiments/Tries/data/analysis_indv.jl
+++ Tesis/lammps/inSilicoHydroGels/experiments/Tries/data/analysis_indv.jl
@@ -40,13 +40,13 @@ data=getData(dirs,file_name);
 
 # Re-organize the information
 energy_assembly=map(s->data[s][1],eachindex(data));
-wcaPair_assembly=map(s->data[s][2],eachindex(data));
-patchPair_assembly=map(s->data[s][3],eachindex(data));
-swapPair_assembly=map(s->data[s][4],eachindex(data));
+wcaPair_assembly=reduce(hcat,map(s->data[s][2][2,:],eachindex(data)));
+patchPair_assembly=reduce(hcat,map(s->data[s][3][2,:],eachindex(data)));
+swapPair_assembly=reduce(hcat,map(s->data[s][4][2,:],eachindex(data)));
 energy_shear=map(s->data[s][5],eachindex(data));
-wcaPair_shear=map(s->data[s][6],eachindex(data));
-patchPair_shear=map(s->data[s][7],eachindex(data));
-swapPair_shear=map(s->data[s][8],eachindex(data));
+wcaPair_shear=reduce(hcat,map(s->data[s][6][2,:],eachindex(data)));
+patchPair_shear=reduce(hcat,map(s->data[s][7][2,:],eachindex(data)));
+swapPair_shear=reduce(hcat,map(s->data[s][8][2,:],eachindex(data)));
 stress_shear=map(s->data[s][9],eachindex(data));
 
 
@@ -77,7 +77,7 @@ T_shear=reduce(hcat,map(s->energy_shear[s][5,:],eachindex(energy_assembly)));
 Tcp_shear=reduce(hcat,map(s->energy_shear[s][2,:],eachindex(energy_assembly)));
 U_shear=reduce(hcat,map(s->energy_shear[s][3,:],eachindex(energy_assembly)));
 K_shear=reduce(hcat,map(s->energy_shear[s][4,:],eachindex(energy_assembly)));
-Eng_shear=reduce(hcat,map(s->U_shear[s].+K_shear[s],eachindex(T_shear)));
+Eng_shear=U_shear.+K_shear;
 
 """
 
@@ -93,11 +93,11 @@ auxsdir=map(s->s[3],split.(dirs,"/"));
 #labels_CL=string.(first.(split.(dirs,"/"))," CL=",aux_CL,"%");
 labels_CL=string.(auxsdir," CL=",aux_CL,"%");
 
-
+# Temperature
 
 tf=last(time_deform);
 
-fig_Temp=Figure(size=(1200,980));
+fig_Temp=Figure(size=(1920,1080));
 ax_leg=Axis(fig_Temp[1:2,3],limits=(0.01,0.1,0.01,0.1))
 hidespines!(ax_leg)
 hidedecorations!(ax_leg)
@@ -142,7 +142,6 @@ vlines!(ax_tcp,last(time_assembly),linestyle=:dash,color=:black)
 
 series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
 
-
 Legend(fig_Temp[1:2,3],ax_leg,
        framevisible=true,
        halign=:center,
@@ -150,14 +149,204 @@ Legend(fig_Temp[1:2,3],ax_leg,
        L"\mathrm{Concentration~and~damp}",
        patchsize=(35,35)
       )
-#                       L"\mathrm{Damps~and~Concentration}",
-#                       framevisible=true,
-#                       orientation=:vertical,
-#                       [elm1],
-#                       ["ede"],
-#                       patchsize=(35,35)#lblsAss_CL
-#                      )
+
+# Total energy and Log
+
+fig_Energy=Figure(size=(1920,1080));
+ax_leg=Axis(fig_Energy[1:2,3],limits=(0.01,0.1,0.01,0.1))
+hidespines!(ax_leg)
+hidedecorations!(ax_leg)
+ax_E = Axis(fig_Energy[1,1:2],
+        title = L"\mathrm{Total~energy}",
+        xlabel = L"\mathrm{Time~unit}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+        #xscale = log10,
+        #limits = (10e0,exp10(round(log10(tf))),nothing,nothing)
+    )
+ax_Elo = Axis(fig_Energy[2,1:2],
+        title = L"\mathrm{Total~Energy}",
+        xlabel = L"\mathrm{Time~steps}~\ln_{10}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+        xscale = log10,
+        limits = (10e0,exp10(round(log10( length(time_assembly)+length(time_deform)))),nothing,nothing)
+    )
+
+println("Plotting lines")
+
+series!(ax_E,time_assembly,Eng_assembly',color=:tab10)
+series!(ax_E,time_deform,Eng_shear',color=:tab10)
+vlines!(ax_E,last(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_Elo,eachindex(time_assembly),Eng_assembly',color=:tab10)
+series!(ax_Elo,eachindex(time_deform).+length(time_assembly),Eng_shear',color=:tab10)
+vlines!(ax_Elo,length(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+
+println("Legends")
+
+Legend(fig_Energy[1:2,3],ax_leg,
+       framevisible=true,
+       halign=:center,
+       orientation=:vertical,
+       L"\mathrm{Concentration~and~damp}",
+       patchsize=(35,35)
+      )
+
+# Potential energy
+
+fig_EngPot=Figure(size=(1920,1080));
+ax_leg=Axis(fig_EngPot[1:2,5],limits=(0.01,0.1,0.01,0.1))
+hidespines!(ax_leg)
+hidedecorations!(ax_leg)
+ax_Ut = Axis(fig_EngPot[1,1:2],
+        title = L"\mathrm{Potential~energy}",
+        xlabel = L"\mathrm{Time~unit}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+    )
+ax_wca = Axis(fig_EngPot[2,1:2],
+        title = L"\mathrm{WCA~potential}",
+        xlabel = L"\mathrm{Time~steps}~\ln_{10}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+    )
+ax_patch = Axis(fig_EngPot[1,3:4],
+        title = L"\mathrm{Patch~interaction~potential}",
+        xlabel = L"\mathrm{Time~steps}~\ln_{10}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+    )
+ax_swap = Axis(fig_EngPot[2,3:4],
+        title = L"\mathrm{Swap~potential}",
+        xlabel = L"\mathrm{Time~steps}~\ln_{10}",
+        ylabel = L"\mathrm{Energy}",
+        titlesize = 24.0f0,
+        xticklabelsize = 18.0f0,
+        yticklabelsize = 18.0f0,
+        xlabelsize = 20.0f0,
+        ylabelsize = 20.0f0,
+        xminorticksvisible = true, 
+        xminorgridvisible = true,
+        xminorticks = IntervalsBetween(5),
+    )
 
 
+println("Plotting lines")
+
+series!(ax_Ut,time_assembly,U_assembly',color=:tab10)
+series!(ax_Ut,time_deform,U_shear',color=:tab10)
+vlines!(ax_Ut,last(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_wca,time_assembly,wcaPair_assembly',color=:tab10)
+series!(ax_wca,time_deform,wcaPair_shear',color=:tab10)
+vlines!(ax_wca,last(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_patch,time_assembly,patchPair_assembly',color=:tab10)
+series!(ax_patch,time_deform,patchPair_shear',color=:tab10)
+vlines!(ax_patch,last(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_swap,time_assembly,swapPair_assembly',color=:tab10)
+series!(ax_swap,time_deform,swapPair_shear',color=:tab10)
+vlines!(ax_swap,last(time_assembly),linestyle=:dash,color=:black)
+
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+
+println("Legends")
+
+Legend(fig_EngPot[1:2,5],ax_leg,
+       framevisible=true,
+       halign=:center,
+       orientation=:vertical,
+       L"\mathrm{Concentration~and~damp}",
+       patchsize=(35,35)
+      )
+
+# Stress
+
+time_stress=parameters[1][13].*stress_shear[1][1,:];
+stressXX=reduce(hcat,map(s->-stress_shear[s][3,:],eachindex(stress_shear)));
+stressXY=reduce(hcat,map(s->-stress_shear[s][6,:],eachindex(stress_shear)));
+
+
+
+fig_Stress = Figure(size=(1080,980));
+ax_stressXX = Axis(fig_Stress[1,1:2],
+                   title = L"\mathrm{Stress}~xx",
+                   xlabel = L"\mathrm{Time [tau]}",
+                   ylabel = L"\sigma",
+                   titlesize = 24.0f0,
+                   xticklabelsize = 18.0f0,
+                   yticklabelsize = 18.0f0,
+                   xlabelsize = 20.0f0,
+                   ylabelsize = 20.0f0,
+                   xminorticksvisible = true, 
+                   xminorgridvisible = true,
+                   xminorticks = IntervalsBetween(5),
+                   #xscale = log10,
+                   #limits = (10e0,exp10(1+round(log10(tf))),nothing,nothing)
+                  )
+ax_stressXY = Axis(fig_Stress[2,1:2],
+                   title = L"\mathrm{Stress}~xy",
+                   xlabel = L"\mathrm{Time [tau]}",
+                   ylabel = L"\sigma",
+                   titlesize = 24.0f0,
+                   xticklabelsize = 18.0f0,
+                   yticklabelsize = 18.0f0,
+                   xlabelsize = 20.0f0,
+                   ylabelsize = 20.0f0,
+                   xminorticksvisible = true, 
+                   xminorgridvisible = true,
+                   xminorticks = IntervalsBetween(5),
+                   #xscale = log10,
+                   #limits = (10e0,exp10(1+round(log10(tf))),nothing,nothing)
+                  )
+
+series!(ax_stressXX,time_stress,stressXX',labels=labels_CL)
+#map(s->lines!(ax_stressXX,time_stress[s],stressXX[s],label=labels_CL[s]),eachindex(time_stress))
+vlines!(ax_stressXX,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash)
+axislegend(ax_stressXX,L"\mathrm{Cross-Linker~Concentration}",position=:rt)
+
+series!(ax_stressXY,time_stress,stressXY',labels=labels_CL)
+#map(s->lines!(ax_stressXY,time_stress[s],stressXY[s],label=labels_CL[s]),eachindex(time_stress))
+vlines!(ax_stressXY,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash)
+axislegend(ax_stressXY,L"\mathrm{Cross-Linker~Concentration}",position=:rt)
 
 
