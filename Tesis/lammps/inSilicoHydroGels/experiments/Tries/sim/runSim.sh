a63ea3f01d46640b8dd2c6000182f16228b78d24
FranVT <vazqueztfj@proton.me>
2024-10-07 09:26:57 -0600

    Complete simulation

--- Tesis/lammps/inSilicoHydroGels/experiments/Tries/sim/runSim.sh
+++ Tesis/lammps/inSilicoHydroGels/experiments/Tries/sim/runSim.sh
@@ -2,6 +2,7 @@
 
 env OMP_RUN_THREADS=2 mpirun -np 8 lmp -sf omp -in in.assemblyShear.lmp -var L 9.99857 -var NCL 45 -var NMO 1455 -var seed1 1234 -var seed2 4321 -var seed3 3124 -var steps 6000000 -var tstep 0.001 -var sstep 10000
 
+env OMP_RUN_THREADS=2 mpirun -np 8 lmp -sf omp -in in.deformationShear.lmp -var tstep 0.001 -var sstep 10000 -var shear_rate 0.02 -var max_strain 12 -var Nstep_per_strain 50000 -var shear_it 600000 -var Nsave 500 -var seed3 3124 -var Nave 1000 -var rlxT1 50000 -var rlxT2 100000 -var rlxT3 200000 -var rlxT4 400000
 
 cp -r info ..;
 cd ..;
