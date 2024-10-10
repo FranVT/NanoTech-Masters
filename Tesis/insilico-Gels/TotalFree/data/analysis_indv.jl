fa34b09695923a29257d5823d6397f4fb0d597ec
FranVT <vazqueztfj@proton.me>
2024-10-10 12:31:41 -0600

    Graphs

--- Tesis/insilico-Gels/TotalFree/data/analysis_indv.jl
+++ Tesis/insilico-Gels/TotalFree/data/analysis_indv.jl
@@ -14,7 +14,14 @@ dirs_aux = open("dirs.txt") do f
     reduce(vcat,map(s->split(s," "),readlines(f)))
     end
 
-    dirs=dirs_aux[4:10]; #[2:end];
+    # [1:4]
+    # [5:8]
+    # [9:12]
+
+dirs=dirs_aux; #[2:end];
+#dirs=dirs_aux[5:8];
+#dirs=dirs_aux[9:12];
+#dirs=dirs_aux[1:4];
 
 # File names 
 
@@ -64,6 +71,7 @@ file_name = (
    24 -> Relax time 4
 """
 
+#=
 
 parameters=getParameters(dirs,file_name);
 
@@ -90,8 +98,6 @@ time_rlxf3=time_rlxo3+parameters[1][15]*parameters[1][23];
 time_rlxo4=time_rlxf3+time_rlxaux;
 time_rlxf4=time_rlxo4+parameters[1][15]*parameters[1][24];
 
-
-
 deform1=range(0,parameters[1][20],length=Int64(parameters[1][20]*parameters[1][17]/1000))|>collect;
 rlx1=last(deform1).*ones(Int64(parameters[1][21]/1000));
 deform2=range(last(deform1),last(deform1)+parameters[1][20],length=Int64(parameters[1][20]*parameters[1][17]/1000))|>collect;
@@ -103,16 +109,17 @@ rlx4=last(deform4).*ones(Int64(parameters[1][24]/1000));
 
 gamma=reduce(vcat,[deform1,rlx1,deform2,rlx2,deform3,rlx3,deform4,rlx4]);
 
-
-
-
 # Labels
 lblaux_CL=map(s->Int64.(round(s[3]*100)),parameters);
-lblaux_T=map(s->Int64.(round(s[5])),parameters);
-lblaux_damp=map(s->Int64.(round(s[6])),parameters);
+lblaux_T=map(s->s[5],parameters);
+lblaux_damp=map(s->s[6],parameters);
 #auxsdir=map(s->s[2],split.(dirs,"/"));
 #labels_CL=string.(first.(split.(dirs,"/"))," CL=",aux_CL,"%");
-labels_CL=string.("T= ",lblaux_T,", CL= ",lblaux_CL,"%, damp= ",lblaux_damp);
+labels_CL=string.("T=",lblaux_T,", CL=",lblaux_CL,"%, damp=",lblaux_damp);
+
+=#
+
+csh=:tab20;
 
 # Temperature
 
@@ -153,15 +160,15 @@ ax_tcp = Axis(fig_Temp[2,1:2],
         #limits = (10e0,exp10(round(log10(tf))),nothing,nothing)
     )
 
-series!(ax_t,time_assembly,reduce(hcat,map(s->s[1],data_assembly))',color=:tab10)
-series!(ax_t,time_deform,reduce(hcat,map(s->s[1],data_shear))',color=:tab10)
+series!(ax_t,time_assembly,reduce(hcat,map(s->s[1],data_assembly))',color=csh)
+series!(ax_t,time_deform,reduce(hcat,map(s->s[1],data_shear))',color=csh)
 vlines!(ax_t,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_tcp,time_assembly,reduce(hcat,map(s->s[2],data_assembly))',color=:tab10)
-series!(ax_tcp,time_deform,reduce(hcat,map(s->s[2],data_shear))',color=:tab10)
+series!(ax_tcp,time_assembly,reduce(hcat,map(s->s[2],data_assembly))',color=csh)
+series!(ax_tcp,time_deform,reduce(hcat,map(s->s[2],data_shear))',color=csh)
 vlines!(ax_tcp,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 Legend(fig_Temp[1:2,3],ax_leg,
        framevisible=true,
@@ -212,15 +219,15 @@ ax_Elo = Axis(fig_Energy[2,1:2],
 
 println("Plotting lines")
 
-series!(ax_E,time_assembly,reduce(hcat,map(s->s[5],data_assembly))',color=:tab10)
-series!(ax_E,time_deform,reduce(hcat,map(s->s[5],data_shear))',color=:tab10)
+series!(ax_E,time_assembly,reduce(hcat,map(s->s[5],data_assembly))',color=csh)
+series!(ax_E,time_deform,reduce(hcat,map(s->s[5],data_shear))',color=csh)
 vlines!(ax_E,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_Elo,eachindex(time_assembly),reduce(hcat,map(s->s[5],data_assembly))',color=:tab10)
-series!(ax_Elo,eachindex(time_deform).+length(time_assembly),reduce(hcat,map(s->s[5],data_shear))',color=:tab10)
+series!(ax_Elo,eachindex(time_assembly),reduce(hcat,map(s->s[5],data_assembly))',color=csh)
+series!(ax_Elo,eachindex(time_deform).+length(time_assembly),reduce(hcat,map(s->s[5],data_shear))',color=csh)
 vlines!(ax_Elo,length(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 println("Legends")
 
@@ -295,23 +302,23 @@ ax_swap = Axis(fig_EngPot[2,3:4],
 
 println("Plotting lines")
 
-series!(ax_Ut,time_assembly,reduce(hcat,map(s->s[3],data_assembly))',color=:tab10)
-series!(ax_Ut,time_deform,reduce(hcat,map(s->s[3],data_shear))',color=:tab10)
+series!(ax_Ut,time_assembly,reduce(hcat,map(s->s[3],data_assembly))',color=csh)
+series!(ax_Ut,time_deform,reduce(hcat,map(s->s[3],data_shear))',color=csh)
 vlines!(ax_Ut,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_wca,time_assembly,reduce(hcat,map(s->s[6],data_assembly))',color=:tab10)
-series!(ax_wca,time_deform,reduce(hcat,map(s->s[6],data_shear))',color=:tab10)
+series!(ax_wca,time_assembly,reduce(hcat,map(s->s[6],data_assembly))',color=csh)
+series!(ax_wca,time_deform,reduce(hcat,map(s->s[6],data_shear))',color=csh)
 vlines!(ax_wca,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_patch,time_assembly,reduce(hcat,map(s->s[7],data_assembly))',color=:tab10)
-series!(ax_patch,time_deform,reduce(hcat,map(s->s[7],data_shear))',color=:tab10)
+series!(ax_patch,time_assembly,reduce(hcat,map(s->s[7],data_assembly))',color=csh)
+series!(ax_patch,time_deform,reduce(hcat,map(s->s[7],data_shear))',color=csh)
 vlines!(ax_patch,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_swap,time_assembly,reduce(hcat,map(s->s[8],data_assembly))',color=:tab10)
-series!(ax_swap,time_deform,reduce(hcat,map(s->s[8],data_shear))',color=:tab10)
+series!(ax_swap,time_assembly,reduce(hcat,map(s->s[8],data_assembly))',color=csh)
+series!(ax_swap,time_deform,reduce(hcat,map(s->s[8],data_shear))',color=csh)
 vlines!(ax_swap,last(time_assembly),linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 println("Legends")
 
@@ -387,19 +394,19 @@ ax_swap = Axis(fig_PotDef[2,3:4],
 
 println("Plotting lines")
 
-series!(ax_Ut,time_deform.-last(time_assembly),reduce(hcat,map(s->s[3],data_shear))',color=:tab10)
+series!(ax_Ut,time_deform.-last(time_assembly),reduce(hcat,map(s->s[3],data_shear))',color=csh)
 vlines!(ax_Ut,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_wca,time_deform.-last(time_assembly),reduce(hcat,map(s->s[6],data_shear))',color=:tab10)
+series!(ax_wca,time_deform.-last(time_assembly),reduce(hcat,map(s->s[6],data_shear))',color=csh)
 vlines!(ax_wca,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_patch,time_deform.-last(time_assembly),reduce(hcat,map(s->s[7],data_shear))',color=:tab10)
+series!(ax_patch,time_deform.-last(time_assembly),reduce(hcat,map(s->s[7],data_shear))',color=csh)
 vlines!(ax_patch,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_swap,time_deform.-last(time_assembly),reduce(hcat,map(s->s[8],data_shear))',color=:tab10)
+series!(ax_swap,time_deform.-last(time_assembly),reduce(hcat,map(s->s[8],data_shear))',color=csh)
 vlines!(ax_swap,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 Legend(fig_PotDef[1:2,5],ax_leg,
        framevisible=true,
@@ -443,13 +450,13 @@ ax_stressXY = Axis(fig_Stress[2,1:2],
                    xminorticks = IntervalsBetween(5),
                   )
 
-series!(ax_stressXX,time_shear,reduce(hcat,map(s->s[12],data_shear))',color=:tab10)
+series!(ax_stressXX,time_shear,reduce(hcat,map(s->s[12],data_shear))',color=csh)
 vlines!(ax_stressXX,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_stressXY,time_shear,reduce(hcat,map(s->s[13],data_shear))',color=:tab10)
+series!(ax_stressXY,time_shear,reduce(hcat,map(s->s[13],data_shear))',color=csh)
 vlines!(ax_stressXY,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 println("Legends")
 
@@ -461,16 +468,16 @@ Legend(fig_Stress[1:2,3],ax_leg,
        patchsize=(35,35)
       )
 
-#=
+
 # Holap
 fig_Displ = Figure(size=(1080,980));
 ax_leg=Axis(fig_Displ[1:2,3],limits=(0.01,0.1,0.01,0.1))
 hidespines!(ax_leg)
 hidedecorations!(ax_leg)
 ax_DisplCl = Axis(fig_Displ[1,1:2],
-                   title = L"\mathrm{Mean~displacement of Cross Linkers}",
+                   title = L"\mathrm{Mean~displacement~of~Cross~Linkers~during~shear}",
                    xlabel = L"\mathrm{Time [tau]}",
-                   ylabel = L"\sigma",
+                   ylabel = L"\langle d \rangle",
                    titlesize = 24.0f0,
                    xticklabelsize = 18.0f0,
                    yticklabelsize = 18.0f0,
@@ -480,10 +487,10 @@ ax_DisplCl = Axis(fig_Displ[1,1:2],
                    xminorgridvisible = true,
                    xminorticks = IntervalsBetween(5),
                   )
-ax_DisplCl = Axis(fig_Displ[1,1:2],
-                   title = L"\mathrm{Mean~displacement of Cross Linkers}",
+ax_DisplMo = Axis(fig_Displ[2,1:2],
+                   title = L"\mathrm{Mean~displacement~of~Monomers~during~shear}",
                    xlabel = L"\mathrm{Time [tau]}",
-                   ylabel = L"\sigma",
+                   ylabel = L"\langle d \rangle",
                    titlesize = 24.0f0,
                    xticklabelsize = 18.0f0,
                    yticklabelsize = 18.0f0,
@@ -493,10 +500,10 @@ ax_DisplCl = Axis(fig_Displ[1,1:2],
                    xminorgridvisible = true,
                    xminorticks = IntervalsBetween(5),
                   )
-ax_DisplCl = Axis(fig_Displ[1,1:2],
-                   title = L"\mathrm{Mean~displacement of Cross Linkers}",
+ax_DisplCM = Axis(fig_Displ[3,1:2],
+                   title = L"\mathrm{Mean~displacement~of~Central~Particles~during~shear}",
                    xlabel = L"\mathrm{Time [tau]}",
-                   ylabel = L"\sigma",
+                   ylabel = L"\langle d \rangle",
                    titlesize = 24.0f0,
                    xticklabelsize = 18.0f0,
                    yticklabelsize = 18.0f0,
@@ -507,23 +514,25 @@ ax_DisplCl = Axis(fig_Displ[1,1:2],
                    xminorticks = IntervalsBetween(5),
                   )
 
+series!(ax_DisplCl,time_shear,reduce(hcat,map(s->s[9],data_shear))',color=csh)
+vlines!(ax_DisplCl,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_stressXX,time_shear,reduce(hcat,map(s->s[12],data_shear))',color=:tab10)
-vlines!(ax_stressXX,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
+series!(ax_DisplMo,time_shear,reduce(hcat,map(s->s[10],data_shear))',color=csh)
+vlines!(ax_DisplMo,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_stressXY,time_shear,reduce(hcat,map(s->s[13],data_shear))',color=:tab10)
-vlines!(ax_stressXY,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
+series!(ax_DisplCM,time_shear,reduce(hcat,map(s->s[11],data_shear))',color=csh)
+vlines!(ax_DisplCM,[time_rlxo1,time_rlxf1,time_rlxo2,time_rlxf2,time_rlxo3,time_rlxf3,time_rlxo4,time_rlxf4],linestyle=:dash,color=:black)
 
-series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=:tab10,labels=labels_CL)
+series!(ax_leg,zeros(length(dirs),length(dirs)),linestyle=:solid,color=csh,labels=labels_CL)
 
 println("Legends")
 
-Legend(fig_Stress[1:2,3],ax_leg,
+Legend(fig_Displ[1:2,3],ax_leg,
        framevisible=true,
        halign=:center,
        orientation=:vertical,
        L"\mathrm{Concentration~and~damp}",
        patchsize=(35,35)
       )
-=#
+
 
