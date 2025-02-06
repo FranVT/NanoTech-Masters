c417f481d93d464016fc07babe8725be4a07ceeb
FranVT <vazqueztfj@proton.me>
2025-02-06 10:26:04 -0600

    Create scripts to graphs the potentials

--- Tesis/auxiliaries/auxs/pot/functions.jl
+++ Tesis/auxiliaries/auxs/pot/functions.jl
@@ -23,6 +23,16 @@ function SwapU(w,eps_ij,eps_ik,eps_jk,sig_p,r_ij,r_ik,r_c)
     return round(w.*eps_jk.*U3(eps_ij,eps_jk,sig_p,r_ij).*U3(eps_ik,eps_jk,sig_p,r_ik),digits=2^7)
 end
 
+function DiffU3(eps_pair,eps_3,sig_p,r)
+"""
+    Get the central finite difference given the value of the position and the function.
+"""
+    dh=1e-3;
+    fo=U3(eps_pair,eps_3,sig_p,r+dh);
+    ff=U3(eps_pair,eps_3,sig_p,r-dh);
+     return (1/(2*dh))*( fo - ff );
+end
+
 function forceSwap(w,eps_ij,eps_ik,eps_jk,sig_p,r_ij,r_ik)
 """
     Compute the force of the swap potential
@@ -42,7 +52,8 @@ function Upatch(eps_pair,sig_p,r)
     Auxiliary potential to create Swap Mechanism based in Patch-Patch interaction
 """
     if r < 1.5*sig_p 
-        return round(2*eps_pair*( ((sig_p^4)./((2).*r.^4)) .-1).*exp.((sig_p)./(r.-(1.5*sig_p)).+2),digits=2^7)
+        #return round(2*eps_pair*( ((sig_p^4)./((2).*r.^4)) .-1).*exp.(((sig_p)./(r.-(1.5*sig_p))).+2),digits=2^7)
+        return 2*eps_pair*( sig_p^4/(2*r^4) - 1 )*exp( sig_p/(r-1.5*sig_p) + 2 )
     else
         return 0.0
     end
--- Tesis/auxiliaries/auxs/pot/potentials.jl
+++ Tesis/auxiliaries/auxs/pot/potentials.jl
@@ -9,6 +9,7 @@ include("functions.jl")
 
 # Parameters
 N = 100;
+M = 7;
 rad_pat = 0.5;
 rad_pac = 0.2;
 
@@ -20,6 +21,8 @@ eps_ij = 1;
 eps_ik = 1;
 eps_jk = 1;
 
+w = 2;
+
 # Distance domain
 r_pat = (sig_pat/1000,sig_pat*2^(1/6));
 r_pac = (sig_pac/1000,1.5*sig_pac);
@@ -28,16 +31,18 @@ r_pac = (sig_pac/1000,1.5*sig_pac);
 wca_eval = map(r->WCA(eps_wca,sig_pat,r),range(r_pat...,length=100));
 wcaF_eval = map(r->-DiffWCAEval(eps_wca,sig_pat,r),range(r_pat...,length=100));
 
-_eval = map(r->WCA(eps_wca,sig_pat,r),range(r_pat...,length=100));
-F_eval = map(r->-DiffWCAEval(eps_wca,sig_pat,r),range(r_pat...,length=100));
+patch_eval = map(r->Upatch(eps_ij,sig_pac,r),range(r_pac...,length=100));
+patchF_eval = map(r->-DiffUpatchEval(eps_ij,sig_pac,r),range(r_pac...,length=100));
 
+swap_eval = map(r_ik->map(r_ij->SwapU(w,eps_ij,eps_ik,eps_jk,sig_pac,r_ij,r_ik,1.5*sig_pac),range(r_pac...,length=100)),range(r_pac...,length=M));
+swapF_eval = map(r_ik->map(r_ij->forceSwap(w,eps_ij,eps_ik,eps_jk,sig_pac,r_ij,r_ik),range(r_pac...,length=100)),range(r_pac...,length=M));
 
 
 
 fig=Figure(size=(1080,920));
-ax = Axis(fig[1:2,1:2],
-            title = L"\mathrm{WCA~potential}",
-            xlabel = L"\mathrm{Distance}",
+ax_pot = Axis(fig[1:2,1:2],
+            title = L"\mathrm{Potential~and~forces}",
+            xlabel = L"\mathrm{Distance} i-j",
             titlesize = 24.0f0,
             xticklabelsize = 18.0f0,
             yticklabelsize = 18.0f0,
@@ -46,12 +51,43 @@ ax = Axis(fig[1:2,1:2],
             xminorticksvisible = true, 
             xminorgridvisible = true,
             xminorticks = IntervalsBetween(5),
-            limits=(r_pat...,-10,10)
+            limits=(r_pac...,-3,3)
             #xlims = r_pat,
             #ylims = (-10,10)
          )
-lines!(ax,range(r_pat...,length=100),wca_eval,label=L"\mathrm{WCA}(r)")
-lines!(ax,range(r_pat...,length=100),wcaF_eval,label=L"\partial_r\mathrm{WCA}(r)")
-axislegend(ax,position = :lb,orientation=:horizontal)
+ax_force = Axis(fig[3:4,1:2],
+            title = L"\mathrm{Forces}",
+            xlabel = L"\mathrm{Distance} i-j",
+            titlesize = 24.0f0,
+            xticklabelsize = 18.0f0,
+            yticklabelsize = 18.0f0,
+            xlabelsize = 20.0f0,
+            ylabelsize = 20.0f0,
+            xminorticksvisible = true, 
+            xminorgridvisible = true,
+            xminorticks = IntervalsBetween(5),
+            limits=(r_pac...,-20,20)
+            #xlims = r_pat,
+            #ylims = (-10,10)
+         )
+
+
+
+#lines!(ax,range(r_pat...,length=100),wca_eval,label=L"\mathrm{WCA}(r)")
+#lines!(ax,range(r_pat...,length=100),wcaF_eval,label=L"\partial_r\mathrm{WCA}(r)")
+lines!(ax_pot,range(r_pac...,length=100),patch_eval,label=L"\mathrm{U}_{\mathrm{patch}}(r_{ij},r_{ik})")
+lines!(ax_force,range(r_pac...,length=100),patchF_eval,label=L"\partial_r\mathrm{U}_{\mathrm{patch}}(r)")
+series!(ax_pot,range(r_pac...,length=100),reduce(hcat,swap_eval)',
+        labels=[L"\mathrm{U}_{\mathrm{patch}}(r_{ij},%$i) " for i in round.(range(r_pac...,length=M)./sig_pac,digits=3)]
+       )
+series!(ax_force,range(r_pac...,length=100),reduce(hcat,swapF_eval)',
+        labels=[L"\partial_r\mathrm{U}_{\mathrm{patch}}(r_{ij},%$i) " for i in round.(range(r_pac...,length=M)./sig_pac,digits=3)]
+       )
+
+
+
+axislegend(ax_pot,position = :lb,orientation=:horizontal,nbanks=4,labelsize=20)
+axislegend(ax_force,position = :lb,orientation=:horizontal,nbanks=4,labelsize=20)
+
 
 
