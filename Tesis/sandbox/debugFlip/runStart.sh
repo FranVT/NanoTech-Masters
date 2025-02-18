f162ac1616c12ad7e8403e57486a7b9d8bdfc505
FranVT <vazqueztfj@proton.me>
2025-02-18 17:07:18 -0600

    Test with more flips

--- Tesis/sandbox/debugFlip/runStart.sh
+++ Tesis/sandbox/debugFlip/runStart.sh
@@ -16,7 +16,7 @@ for var_shearRate in 0.01; #0.01 0.001;
 do
 for var_cCL in 0.1; #0.06 0.1;
 do 
-for Nexp in 57; #$(seq 1 5);
+for Nexp in 58; #$(seq 1 5);
 do
 
 # Cifras significativas
@@ -62,7 +62,7 @@ sstep_defor=10000;
 
 # Shear rate parameters
 shear_rate=$var_shearRate;
-max_strain=1;
+max_strain=3;
 Nstep_per_strain=$(echo "scale=$cs; $(echo "scale=$cs; 1 / $shear_rate" | bc) * $(echo "scale=$cs; 1 / $tstep_defor" | bc)" | bc) ;
 Nstep_per_strain=${Nstep_per_strain%.*};
 
