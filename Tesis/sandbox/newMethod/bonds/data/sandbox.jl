8d48d51b50e275c4aaf79ac829cdd6ad964833a3
FranVT <vazqueztfj@proton.me>
2025-03-22 17:28:52 -0600

    Update graphs script

--- Tesis/sandbox/newMethod/bonds/data/sandbox.jl
+++ Tesis/sandbox/newMethod/bonds/data/sandbox.jl
@@ -39,6 +39,95 @@ fig_temp=plot(
 plot!(df_new."time-step".*df_assembly."TimeStep",df_assembly."Temp",label=L"\mathrm{Assembly}");
 plot!(df_new."time-step".*(last(df_assembly."TimeStep") .+ df_shear."TimeStep"),df_shear."Temp",label=L"\mathrm{Shear}");
 
+# Stress figure Norm
+
+# Virial
+stress_ass = sqrt.( df_stressA."xx".^2 .+ df_stressA."yy".^2 .+ df_stressA."zz".^2 .+ (2).*(df_stressA."xy".^2 .+ df_stressA."xz".^2 .+ df_stressA."yz".^2)  );
+stress_she = sqrt.( df_stressS."xx".^2 .+ df_stressS."yy".^2 .+ df_stressS."zz".^2 .+ (2).*(df_stressS."xy".^2 .+ df_stressS."xz".^2 .+ df_stressS."yz".^2)  );
+
+pressure_ass = (1/3).*(df_stressA."xx" .+ df_stressA."yy" .+ df_stressA."zz");
+pressure_she = (1/3).*(df_stressS."xx" .+ df_stressS."yy" .+ df_stressS."zz");
+
+
+# stress/atom all
+stress_ass_atom = sqrt.( df_stressA."xx_atom".^2 .+ df_stressA."yy_atom".^2 .+ df_stressA."zz_atom".^2 .+ (2).*(df_stressA."xy_atom".^2 .+ df_stressA."xz_atom".^2 .+ df_stressA."yz_atom".^2)  );
+stress_she_atom = sqrt.( df_stressS."xx_atom".^2 .+ df_stressS."yy_atom".^2 .+ df_stressS."zz_atom".^2 .+ (2).*(df_stressS."xy_atom".^2 .+ df_stressS."xz_atom".^2 .+ df_stressS."yz_atom".^2)  );
+
+pressure_ass_atom = (1/3).*(df_stressA."xx_atom" .+ df_stressA."yy_atom" .+ df_stressA."zz_atom");
+pressure_she_atom = (1/3).*(df_stressS."xx_atom" .+ df_stressS."yy_atom" .+ df_stressS."zz_atom");
+
+
+# stress/atom patches
+stress_ass_patch = sqrt.( df_stressA."xx_patch".^2 .+ df_stressA."yy_patch".^2 .+ df_stressA."zz_patch".^2 .+ (2).*(df_stressA."xy_patch".^2 .+ df_stressA."xz_patch".^2 .+ df_stressA."yz_patch".^2)  );
+stress_she_patch = sqrt.( df_stressS."xx_patch".^2 .+ df_stressS."yy_patch".^2 .+ df_stressS."zz_patch".^2 .+ (2).*(df_stressS."xy_patch".^2 .+ df_stressS."xz_patch".^2 .+ df_stressS."yz_patch".^2)  );
+
+pressure_ass_patch = (1/3).*(df_stressA."xx_patch" .+ df_stressA."yy_patch" .+ df_stressA."zz_patch");
+pressure_she_patch = (1/3).*(df_stressS."xx_patch" .+ df_stressS."yy_patch" .+ df_stressS."zz_patch");
+
+
+# stress/atom central particles 
+stress_ass_central = sqrt.( df_stressA."xx_part".^2 .+ df_stressA."yy_part".^2 .+ df_stressA."zz_part".^2 .+ (2).*(df_stressA."xy_part".^2 .+ df_stressA."xz_part".^2 .+ df_stressA."yz_part".^2)  );
+stress_she_central = sqrt.( df_stressS."xx_part".^2 .+ df_stressS."yy_part".^2 .+ df_stressS."zz_part".^2 .+ (2).*(df_stressS."xy_part".^2 .+ df_stressS."xz_part".^2 .+ df_stressS."yz_part".^2)  );
+
+pressure_ass_central = (1/3).*(df_stressA."xx_part" .+ df_stressA."yy_part" .+ df_stressA."zz_part");
+pressure_she_central = (1/3).*(df_stressS."xx_part" .+ df_stressS."yy_part" .+ df_stressS."zz_part");
+
+
+# Stress xy 
+fig_stress_xy=plot(
+                title=[L"\mathrm{Pressure~Virial}" L"\mathrm{Stress-all}" L"\mathrm{Stress-patch}" L"\mathrm{Stress-central}"],
+                xlabel=L"\mathrm{LJ}~\tau",
+                ylabel=L"\sigma_{xy}",
+                legend_position=:bottomright,
+                framestyle=:box,
+                formatter=:scientific,
+                size=(1050,788),
+                layout=4,
+            );
+plot!(df_new."time-step".*df_stressA."TimeStep",[-df_stressA."xy" -df_stressA."xy_atom" -df_stressA."xy_patch" -df_stressA."xy_part"],
+        label=repeat([L"\mathrm{Assembly}"],1,4)
+       );
+plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),[-df_stressS."xy" -df_stressS."xy_atom" -df_stressS."xy_patch" -df_stressS."xy_part"],
+        label=repeat([L"\mathrm{Shear}"],1,4)
+       );
+
+# Stress norm 
+fig_stress=plot(
+                title=[L"\mathrm{Pressure~Virial}" L"\mathrm{Stress-all}" L"\mathrm{Stress-patch}" L"\mathrm{Stress-central}"],
+                xlabel=L"\mathrm{LJ}~\tau",
+                ylabel=L"|\sigma|",
+                legend_position=:bottomright,
+                framestyle=:box,
+                formatter=:scientific,
+                size=(1050,788),
+                layout=4,
+            );
+plot!(df_new."time-step".*df_stressA."TimeStep",[-stress_ass -stress_ass_atom -stress_ass_patch -stress_ass_central],
+        label=repeat([L"\mathrm{Assembly}"],1,4)
+       );
+plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),[-stress_she -stress_she_atom -stress_she_patch -stress_she_central],
+        label=repeat([L"\mathrm{Shear}"],1,4)
+       );
+
+
+# Pressure from Stress tensor
+fig_pressure=plot(
+                title=[L"\mathrm{Pressure}" L"\mathrm{Stress-all}" L"\mathrm{Stress-patch}" L"\mathrm{Stress-central}"],
+                xlabel=L"\mathrm{LJ}~\tau",
+                ylabel=L"p",
+                legend_position=:bottomright,
+                framestyle=:box,
+                formatter=:scientific,
+                size=(1050,788),
+                layout=4,
+            );
+plot!(df_new."time-step".*df_stressA."TimeStep",[pressure_ass pressure_ass_atom pressure_ass_patch pressure_ass_central],
+        label=repeat([L"\mathrm{Assembly}"],1,4)
+       );
+plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),[pressure_she pressure_she_atom pressure_she_patch pressure_she_central],
+        label=repeat([L"\mathrm{Shear}"],1,4)
+       );
+
 # Pressure 
 fig_p=plot(
                 title=L"\mathrm{Pressure}",
@@ -65,102 +154,6 @@ fig_h=plot(
 plot!(df_new."time-step".*df_assembly."TimeStep",df_assembly."H",label=L"\mathrm{Assembly}");
 plot!(df_new."time-step".*(last(df_assembly."TimeStep") .+ df_shear."TimeStep"),df_shear."H",label=L"\mathrm{Shear}");
 
-# Stress 
-fig_stress_xy=plot(
-                title=L"\mathrm{Stress}~xy",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"\sigma_{xy}",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",-df_stressA."xy",label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),-df_stressS."xy",label=L"\mathrm{Shear}");
-
-
-stress_ass = sqrt.( df_stressA."xx".^2 .+ df_stressA."yy".^2 .+ df_stressA."zz".^2 .+ (2).*(df_stressA."xy".^2 .+ df_stressA."xz".^2 .+ df_stressA."yz".^2)  );
-stress_she = sqrt.( df_stressS."xx".^2 .+ df_stressS."yy".^2 .+ df_stressS."zz".^2 .+ (2).*(df_stressS."xy".^2 .+ df_stressS."xz".^2 .+ df_stressS."yz".^2)  );
-
-pressure_ass = (1/3).*(df_stressA."xx" .+ df_stressA."yy" .+ df_stressA."zz");
-pressure_she = (1/3).*(df_stressS."xx" .+ df_stressS."yy" .+ df_stressS."zz");
-
-# Stress 
-fig_stress=plot(
-                title=L"\mathrm{Stress~Tensor}",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"\sigma",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",stress_ass,label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),stress_she,label=L"\mathrm{Shear}");
-
-# Stress 
-fig_pressure=plot(
-                title=L"\mathrm{Pressure~Tensor}",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"p",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",pressure_ass,label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),pressure_she,label=L"\mathrm{Shear}");
-
-
-# Stress Atom 
-fig_stress_atom_xy=plot(
-                title=L"\mathrm{Stress~Atom}~xy",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"\sigma_{xy}",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",-df_stressA."xy_atom",label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),-df_stressS."xy_atom",label=L"\mathrm{Shear}");
-
-
-stress_ass_atom = sqrt.( df_stressA."xx_atom".^2 .+ df_stressA."yy_atom".^2 .+ df_stressA."zz_atom".^2 .+ (2).*(df_stressA."xy_atom".^2 .+ df_stressA."xz_atom".^2 .+ df_stressA."yz_atom".^2)  );
-stress_she_atom = sqrt.( df_stressS."xx_atom".^2 .+ df_stressS."yy_atom".^2 .+ df_stressS."zz_atom".^2 .+ (2).*(df_stressS."xy_atom".^2 .+ df_stressS."xz_atom".^2 .+ df_stressS."yz_atom".^2)  );
-
-pressure_ass_atom = (1/3).*(df_stressA."xx_atom" .+ df_stressA."yy_atom" .+ df_stressA."zz_atom");
-pressure_she_atom = (1/3).*(df_stressS."xx_atom" .+ df_stressS."yy_atom" .+ df_stressS."zz_atom");
-
-# Stress 
-fig_stress_atom=plot(
-                title=L"\mathrm{Stress~Tensor~Atom}",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"\sigma",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",stress_ass_atom,label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),stress_she_atom,label=L"\mathrm{Shear}");
-
-# Stress 
-fig_pressure_atom=plot(
-                title=L"\mathrm{Pressure~Tensor~Atom}",
-                xlabel=L"\mathrm{LJ}~\tau",
-                ylabel=L"p",
-                legend_position=:bottomright,
-                framestyle=:box,
-                formatter=:scientific,
-                size=(1050,788),
-            );
-plot!(df_new."time-step".*df_stressA."TimeStep",pressure_ass_atom,label=L"\mathrm{Assembly}");
-plot!(df_new."time-step".*(last(df_stressA."TimeStep") .+ df_stressS."TimeStep"),pressure_she_atom,label=L"\mathrm{Shear}");
-
-
-
-
 # Energy 
 fig_energy=plot(
                 title=L"\mathrm{Total~Energy}",
@@ -182,9 +175,5 @@ map(s->savefig(fig_h,s),joinpath.(df_new."main-directory",df_new."imgs-dir","H.p
 map(s->savefig(fig_stress_xy,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress_xy.png"))
 map(s->savefig(fig_stress,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress.png"))
 map(s->savefig(fig_pressure,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress_pressure.png"))
-map(s->savefig(fig_stress_atom_xy,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress_xy_atom.png"))
-map(s->savefig(fig_stress_atom,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress_atom.png"))
-map(s->savefig(fig_pressure_atom,s),joinpath.(df_new."main-directory",df_new."imgs-dir","stress_pressure_atom.png"))
 
 map(s->savefig(fig_energy,s),joinpath.(df_new."main-directory",df_new."imgs-dir","energy-log.png"))
-
--- Tesis/sandbox/newMethod/bonds/runSim.sh
+++ Tesis/sandbox/newMethod/bonds/runSim.sh
@@ -8,7 +8,7 @@
 cs=6;
 
 ## Loops of parameters
-for var_shearRate in 0.1;
+for var_shearRate in 0.01;
 do
     for var_ccL in 0.1;
     do
@@ -23,11 +23,11 @@ do
             # System parameters
             phi=0.5;
             CL_con=$var_ccL;
-            N_particles=500;
+            N_particles=1500;
             shear_rate=$var_shearRate;
             damp=0.5;
             T=0.05;
-            max_strain=2;
+            max_strain=5;
 
             # Numeric parameters
             dt=0.001;
