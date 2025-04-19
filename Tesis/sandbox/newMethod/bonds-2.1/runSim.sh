f30d818fc289d4da2c721b9cd2d4a03ff97d35d1
FranVT <vazqueztfj@proton.me>
2025-04-19 10:22:46 -0600

    Fix stuff new simulation scheme

--- Tesis/sandbox/newMethod/bonds-2.1/runSim.sh
+++ Tesis/sandbox/newMethod/bonds-2.1/runSim.sh
@@ -4,6 +4,9 @@
 
 #!/bin/bash
 
+# Directories
+dir_home=$pwd;
+
 # Significant decimals
 cs=6;
 
@@ -75,6 +78,9 @@ do
     log_name="log-assembly-$(date +%H%M%S-%F).lammps";
     pwd;
     env OMP_RUN_THREADS=1 mpirun -np ${nodes} lmp -sf omp -in in.assembly.lmp -log $log_name -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3  -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $sys_dir_name -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]};
+
+    cd ${sys_dir_name};
+    pwd;
 
     for var_shearRate in 0.01;
     do
@@ -239,22 +245,20 @@ do
             # Directory stuff
             exp_dir_name="$(date +%F-%H%M%S)-Nexp-${Nexp}";
             mkdir ${exp_dir_name}; cd ${exp_dir_name}; mkdir traj;
+            full_path="$pwd/$sys_dir_name/$shear_dir_name/$exp_dir_name";
             
             # Run the shear protocol
-            cd ../../..;
             log_name="log-shear-$(date +%H%M%S-%F).lammps";
             pwd;
-            env OMP_RUN_THREADS=1 mpirun -np ${nodes} lmp -sf omp -in ../../../in.shear.lmp -log $log_name -var dirSys $sys_dir_name -var temp $T -var damp $damp -var tstep $dt -var shear_rate $shear_rate -var max_strain $max_strain -var Nstep_per_strain $Nstep_per_strain -var shear_it $shear_it -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var seed3 $seed3 -var rlxT1 $relaxTime1 -var rlxT2 $relaxTime2 -var rlxT3 $relaxTime3 -var Dir $pwd -var file6_name ${files_name[5]} -var file7_name ${files_name[6]} -var file8_name ${files_name[7]} -var file9_name ${files_name[8]} -var file10_name ${files_name[9]};
-
-            cd ..;
+            cd "$dir_home/sim";
+            env OMP_RUN_THREADS=1 mpirun -np ${nodes} lmp -sf omp -in in.shear.lmp -log $log_name -var dirSys $sys_dir_name -var temp $T -var damp $damp -var tstep $dt -var shear_rate $shear_rate -var max_strain $max_strain -var Nstep_per_strain $Nstep_per_strain -var shear_it $shear_it -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var seed3 $seed3 -var rlxT1 $relaxTime1 -var rlxT2 $relaxTime2 -var rlxT3 $relaxTime3 -var Dir $full_path -var file6_name ${files_name[5]} -var file7_name ${files_name[6]} -var file8_name ${files_name[7]} -var file9_name ${files_name[8]} -var file10_name ${files_name[9]};
+            
         done
-
-        cd ..;
+        
     done
 
-        cd ..;
         mv ${sys_dir_name} ../data;
 
-        cd ..; # Go to the general directory, such taht in we can go into sim directory in line 79
+        cd ..; # Go to the general directory, such that in we can go into sim directory in line 79
 
 done
