f17f97b614eb6aa5cbe36af0e0fa86b528e90251
FranVT <vazqueztfj@proton.me>
2025-04-23 10:40:05 -0600

    Improve the simulation scheme

--- Tesis/cluster/sim-files/version-2.2/src/runSim.sh
+++ Tesis/cluster/sim-files/version-2.2/src/runSim.sh
@@ -4,23 +4,45 @@
 
 #!/bin/bash
 
+cd ..;
 # Main Directories
 dir_home=$(pwd);
-dir_sim="$dir_home/sim";
+dir_src="$dir_home/src";
+dir_sim="$dir_src/sim";
 dir_data="$dir_home/data";
-id=$(date +%F-%H%M%S);
 
-echo $dir_home;
+# Id for the filenames and directories for simulations
+id=$(date +%F-%H%M);
 
 # Clean the simulation directory from previus simulations
-rm -f "$dir_home/sim/*.sge";
+rm -f "$dir_sim/*.sge";
+
+# Go to source directory
+cd $dir_src;
 
 # Get the parameters from the parameter file
-chmod +x load_parameters.sh
-source ./load_parameters.sh parameters.txt
+chmod +x docs/load_parameters.sh
+source docs/load_parameters.sh parameters.txt
 
-# Significant decimals
-cs=6;
+## Loops of parameters
+for var_ccL in 0.5;
+do
+
+    bash $dir_src/create-file_assembly-sge.sh $var_ccL
+    #qsub assembly-$var_ccL.sge
+
+done
+
+
+
+
+
+
+
+
+
+
+
 
 ## Loops of parameters
 for var_ccL in 0.5;
