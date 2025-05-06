9c71a240e1ec148707d5b1ddc68df195647d95e2
FranVT <vazqueztfj@proton.me>
2025-05-06 12:30:11 -0600

    Fix syntax errors and other stuff

--- Tesis/sandbox/newMethod/bonds-3.0/src/docs/create-file_experiment-sge.sh
+++ Tesis/sandbox/newMethod/bonds-3.0/src/docs/create-file_experiment-sge.sh
@@ -29,6 +29,6 @@ source $dir_src/docs/load_parameters.sh $dir_src/docs/assembly$id-$var_ccL.param
 
 # Run the assembly
 cd $dir_sim
-env OMP_RUN_THREADS=1 mpirun -np 6 lmp -sf omp -in in.assembly.lmp -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_system -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]};
+env OMP_RUN_THREADS=1 mpirun -np 6 lmp -sf omp -in in.assembly.lmp -var bin_y $bin_y -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_system -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]} -var file6_name ${files_name[5]};
 EOF
 
---
+++ Tesis/sandbox/newMethod/bonds-3.0/src/system-2025-05-06-121702-CL-0.5.sh
@@ -0,0 +1,18 @@
+echo "Running the system.sh file"
+
+# Recieve the outer parameters
+dir_sim=$1
+dir_src=$2
+dir_system=$3
+id=$4
+var_ccL=$5
+
+# Load the parameters file
+source $dir_src/docs/load_parameters.sh $dir_src/docs/system.parameters 
+
+# Load the config file for assembly
+source $dir_src/docs/load_parameters.sh $dir_src/docs/assembly$id-$var_ccL.parameters
+
+# Run the assembly
+cd $dir_sim
+env OMP_RUN_THREADS=1 mpirun -np 6 lmp -sf omp -in in.assembly.lmp -var bin_y $bin_y -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var tstep $dt -var Nsave $Nsave -var NsaveStress $NsaveStress -var Ndump $Ndump -var steps $steps_isot -var stepsheat $steps_heat -var Dir $dir_system -var file1_name ${files_name[0]} -var file2_name ${files_name[1]} -var file3_name ${files_name[2]} -var file4_name ${files_name[3]} -var file5_name ${files_name[4]};
