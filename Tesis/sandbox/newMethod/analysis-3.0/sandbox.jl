a1fd556cbf1fc2df0c72b852620473e901173201
FranVT <vazqueztfj@proton.me>
2025-05-15 07:50:11 -0600

    Change the write_data to wirte_restart

--- Tesis/sandbox/newMethod/analysis-3.0/sandbox.jl
+++ Tesis/sandbox/newMethod/analysis-3.0/sandbox.jl
@@ -17,7 +17,7 @@ parent_dir=dirname(pwd());
 # Select the siimulation scheme (Version and stuff)
 scheme_dir="bonds-3.0";
 # Select the "system" by id
-id="2025-05-14-082310";
+id="2025-05-14-211246";
 # Select the system by "cross-linker" concentration
 cl_con=0.5;
 
